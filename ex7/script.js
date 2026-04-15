99bf242a4dcfdc2b87584f364ac56d54e1bb6026
b1343055 <B1343055@cgu.edu.tw>
2026-04-15 13:53:44 +0800

    Add files via upload

---
+++ ex7/script.js
@@ -0,0 +1,34 @@
+var container = document.getElementById('container');
+
+window.onload = function() {
+    container.textContent = add_new_chars(3);
+}
+
+function add_new_chars(x, b = true) {
+    var n = x;
+    if (b) {
+        n = Math.floor(Math.random() * x) + 1;
+    }
+    var str = '';
+    for (let i = 0; i < n; i++) {
+        str += String.fromCharCode(97 + Math.floor(Math.random() * 26));
+    }
+    return str;
+};
+
+var counter = 0;
+window.addEventListener("keyup", function(e) {
+    if (e.key.length !== 1) return;
+    var firstone = container.textContent.substring(0, 1);
+    if (e.key == firstone) {
+        container.textContent = container.textContent.substring(1, container.textContent.length);
+        counter = 0;
+    } else {
+        container.textContent += e.key;
+        if (counter++ >= 2) {
+            container.textContent += add_new_chars(6, false);
+            counter = 0;
+        }
+    }
+    container.textContent += add_new_chars(3);
+});
