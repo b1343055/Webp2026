6bea07dd0b27db69340838f999346c9cea162b67
b1343055 <B1343055@cgu.edu.tw>
2026-04-22 22:10:12 +0800

    Create script.js

---
+++ ex9/script.js
@@ -0,0 +1,20 @@
+var dataUrl =
+  "https://api.unsplash.com/photos?client_id=812193ef71ca946e361ed541979a0cfd91e9419a19235fd05f51ea14233f020a&per_page=30";
+function getimg(){
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET",dataUrl,true)
+    xhr.send();
+    xhr.onload = function(){
+        let data = JSON.parse(this.responseText);
+        add_new_img(data);
+    }
+}
+function add_new_img(dataset){
+    let gal = document.getElementById("gallery");
+    dataset.forEach(item => {
+        console.log(item);
+        let img = document.createElement("img");
+        img.setAttribute("src",item.urls.small);
+        gal.appendChild(img);
+    });
+}
