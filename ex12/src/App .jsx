21c5dc47f7cc1eb3796299ed14f9671db37d2576
b1343055 <B1343055@cgu.edu.tw>
2026-05-13 15:18:46 +0800

    Add files via upload

---
+++ ex12/src/App .jsx
@@ -0,0 +1,14 @@
+import Cgu_login  from './Cgu_login'
+// import './App.css'
+function App() {
+
+  return (
+    <div className="App">
+      <div>
+        <Cgu_login />
+      </div>
+    </div>
+  )
+}
+
+export default App
